f836bae89b22cd74a3eefbb1cb7b8370e101a4ba
Swapnil-101 <swapniltiwari9819@gmail.com>
2024-04-02 13:24:05 +0530

    sfsdawd

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,5 +1,5 @@
 export default {
-  // meEndpoint: '/auth/me',
+  meEndpoint: '/auth/me',
   loginEndpoint: '/jwt/login',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
